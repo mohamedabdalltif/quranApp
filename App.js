7db12f53c24afd25b7972efe32e1f43939b4f6fa
mohamedabdalltif <73546135+mohamedabdalltif@users.noreply.github.com>
2022-07-05 23:29:19 +0200

    Update App.js

--- App.js
+++ App.js
@@ -3,12 +3,10 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import HomeScreen from './src/screens/HomeScreen'
 import DetailsScreen from './src/screens/DetailsScreen'
-import Users from './src/screens/Users'
-import ordered_item from './src/screens/ordered_item'
-import User_details from './src/screens/User_details'
+
 const Stack=createNativeStackNavigator();
 import {StatusBar} from "react-native";
-import Seif from './src/screens/Seif';
+
 import  Colors  from "./src/assets/constants/Color";
 
 
@@ -26,19 +24,7 @@ return(
 />
     <Stack.Navigator 
     screenOptions={{headerShown:false}}
-    // screenOptions={{headerShown:false,headerTitleAlign:"center", headerStyle: {
-    //         backgroundColor: '#f4511e',
-            
-    //       },
-    //       headerTintColor: '#fff',
-    //       headerTitleStyle: {
-    //         fontWeight: 'bold',
-    //        },
-          
-           
-         
-        
-    // }}  
+   
     
     >
     <Stack.Screen
@@ -63,10 +49,7 @@ return(
             },
             headerTintColor: '#fff', }}
              />
-{/* 
-<Stack.Screen name="ordered" component={ordered_item}  
-        options={{ title: 'ordered'}}
-             /> */}
+
     </Stack.Navigator>
 
   </NavigationContainer>
@@ -75,55 +58,3 @@ return(
 )
 }
 export default App;
-
-// // import * as React from 'react';
-// import { View, Text,StatusBar } from 'react-native';
-// // import { NavigationContainer } from '@react-navigation/native';
-// // import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-// // import {StatusBar} from "react-native";
-
-     
-
-
-// function HomeScreen() {
-//   return (
-    
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <StatusBar
-//   backgroundColor="#f4511e"
-//   // barStyle={"dark-content"}
-//   // translucent={true}
-//   />
-//       <Text>Home Screen</Text>
-//     </View>
-//   );
-// }
-
-// const Stack = createNativeStackNavigator();
-
-// function App() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         <Stack.Screen
-//           name="Home"
-//           component={HomeScreen}
-//           options={{
-//             title: 'My home',
-//             headerStyle: {
-//               backgroundColor: '#f4511e',
-//             },
-//             headerTintColor: '#fff',
-//             headerBlurEffect:"systemUltraThinMaterialDark",
-//             headerTransparent:true,
-//             presentation:"transparentModal"
-//           }}
-//         />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-// export default App;
-
